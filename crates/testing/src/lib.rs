31816ce8c5c8909b7684169ab429542ee55db5a1
Radina Avramova <99686592+RadinaAvramova@users.noreply.github.com>
2025-03-16 15:07:34 +0200

    Add files via upload

---
+++ crates/testing/src/lib.rs
@@ -0,0 +1,25 @@
+use clap::Command;
+use spacetimedb_cli::Config;
+use spacetimedb_paths::SpacetimePaths;
+
+pub mod modules;
+pub mod sdk;
+
+#[track_caller]
+pub fn invoke_cli(paths: &SpacetimePaths, args: &[&str]) {
+    lazy_static::lazy_static! {
+        static ref RUNTIME: tokio::runtime::Runtime = tokio::runtime::Builder::new_multi_thread()
+            .enable_all()
+            .build()
+            .unwrap();
+        static ref COMMAND: Command = Command::new("spacetime").no_binary_name(true).subcommands(spacetimedb_cli::get_subcommands());
+    }
+    let config = Config::new_with_localhost(paths.cli_config_dir.cli_toml());
+
+    let args = COMMAND.clone().get_matches_from(args);
+    let (cmd, args) = args.subcommand().expect("Could not split subcommand and args");
+
+    RUNTIME
+        .block_on(spacetimedb_cli::exec_subcommand(config, paths, None, cmd, args))
+        .unwrap();
+}
